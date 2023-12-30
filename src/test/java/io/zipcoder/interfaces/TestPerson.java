42ff933273057f47e3c113ed53e0ad2cb87a2de4
Tevin Glover <tevwork.tg@gmail.com>
2023-12-30 13:51:50 -0500

    part 1.2 done

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,32 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+    @Test
+    public void personTest(){
+        Long id = 3l;
+        Person person = new Person(id);
+        Assert.assertNotNull(person);
+    }
+
+    @Test
+    public void person1Test(){
+        Person person = new Person( 2l, "Roger");
+        Assert.assertNotNull(person);
+    }
+    @Test
+    public void personNameTest(){
+        Person person = new Person( 1l, "Roger");
+
+        person.setName("john");
+
+        String actual = person.getName();
+        String expected = "john";
+
+        Assert.assertEquals(expected,actual);
+
+    }
 
 }
